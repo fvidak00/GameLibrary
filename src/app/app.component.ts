ec267b30ef2c6f62f7c12d21b989321566b9fe95
fvidak <fvidak00@fesb.hr>
2020-12-14 20:09:45 +0100

    Sorted it a bit more, check background color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,23 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, Inject, OnInit, Renderer2 } from '@angular/core';
+import { DOCUMENT } from "@angular/common"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'GameLibrary';
 
-  private isDark=false
-
-  @HostBinding("class")
-  get themeMode(){
-    return this.isDark ? "theme-dark":"theme-light"
+  constructor(@Inject(DOCUMENT) private document:Document, private renderer:Renderer2){
   }
 
+  ngOnInit(){
+    this.renderer.setAttribute(this.document.body, "class", "theme-light")
+  }
 
   onSwitchedTheme(isDarkTheme:boolean){
-    this.isDark=isDarkTheme
+    const hostClass=isDarkTheme ? "theme-dark":"theme-light"
+    this.renderer.setAttribute(this.document.body,"class",hostClass)
   }
 }
